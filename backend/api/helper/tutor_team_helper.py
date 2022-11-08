ae777faa43c6b54c49a4febbe1eb55c9d43b73c6
tristan_Bahuaud <tristan.bahuaud@reseau.eseo.fr>
2022-11-08 16:38:35 +0100

    44d5c6k - add DAO tutorTeam names tutor_team_helper in new folder helper

---
+++ backend/api/helper/tutor_team_helper.py
@@ -0,0 +1,32 @@
+
+from base.models import Mentor, Trainee, TeacherInCharge
+
+# class permettant d'aider à la récupération des équipes tutorales
+class TutorTeamHelper:
+    
+    def getAllTutorTeam(serializers):
+        # boucle permettant de récupérer la totalité des équipes tutorales
+        response = []
+        for data in range(len(serializers.data)):
+            # récupération des clées étrangères
+            teacherInChargeID = serializers.data[data]['teacherInCharge']
+            mentorID = serializers.data[data]['mentor']
+            traineeID = serializers.data[data]['trainee']
+
+            # récupération des données liées aux clées étrangères
+            # on aurait pu le faire en une requête dans la base User 
+            # mais d'un point de vue métier ça n'a pas le même sens
+            teacherInCharge = TeacherInCharge.objects.filter(pk=teacherInChargeID).values('first_name', 'last_name', 'email')
+            mentor = Mentor.objects.filter(pk=mentorID).values('first_name', 'last_name', 'email')
+            trainee = Trainee.objects.filter(pk=traineeID).values('first_name', 'last_name', 'email')
+
+            # fabriaction d'un json object equipe tutorale contenant réellement les infos
+            context = {            
+                'mentor': mentor,
+                'trainee': trainee,
+                'teacherInCharge': teacherInCharge
+            }
+            
+            # ajout de l'équipe tutorale récupérée ou autre équipe tutorale
+            response.append(context)
+        return response
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path('mentors', views.getMentor, name='getMentor'),
     path('add-mentors', views.addMentor, name='addMentor'),
-    # FIXME: Les noms de routes devraient être en kebab-case
-    path('TeachersInCharge', views.getTeacherInCharge, name='getTeacherInCharge'),
-    path('Trainees', views.getTrainee, name='getTrainee'),
+
+    path('teachers-in-charge', views.getTeacherInCharge, name='getTeacherInCharge'),
+    path('trainees', views.getTrainee, name='getTrainee'),
     
     path('interviews', views.getInterview, name='getInterview'),
     path('add-interview', views.addInterview, name='addInterview'),
