b66cceddfd97f600f43b92a1c090ca687bffda2d
Joël <hkcodingapp@gmail.com>
2022-12-23 17:37:52 +0100

    ajout component apprentice form

---
+++ frontend/src/app/components/forms/add-apprentice-form/add-apprentice-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-apprentice-form',
+  templateUrl: './add-apprentice-form.component.html',
+  styleUrls: ['./add-apprentice-form.component.scss']
+})
+export class AddApprenticeFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
