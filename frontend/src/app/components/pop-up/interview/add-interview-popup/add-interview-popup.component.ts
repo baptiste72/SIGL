f569f8d568cac107c7c956e803106655182f26ef
Hugo TN <contact@hugotn.fr>
2023-01-14 17:53:31 +0100

    #3fk4nqv fix: Identification problème de build

--- frontend/src/app/components/pop-up/interview/add-interview-popup/add-interview-popup.component.ts
+++ frontend/src/app/components/pop-up/interview/add-interview-popup/add-interview-popup.component.ts
@@ -2,7 +2,6 @@ import { Component, Inject, OnInit, Optional } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { InterviewService } from '@app/services/interview/interview.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { AuthService } from '@app/services/auth/auth.service';
 import { Semester } from '@app/models/Semester';
 import { SemesterService } from '@app/services/semester/semester.service';
 import { ApprenticeService } from '@app/services/apprentice/apprentice.service';
--- frontend/src/test.ts
+++ frontend/src/test.ts
@@ -7,17 +7,6 @@ import {
   platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
 
-declare const require: {
-  context(
-    path: string,
-    deep?: boolean,
-    filter?: RegExp
-  ): {
-    <T>(id: string): T;
-    keys(): string[];
-  };
-};
-
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
