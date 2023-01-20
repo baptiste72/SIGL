de1ddac7624eaf240c7f09840edd59c54b3fb01a
alexandre.nizery <alexandre.nizery@consertotech.pro>
2023-01-20 13:44:54 +0100

    Init fixtures

--- docker/dev/back-dev/django-entrypoint.sh
+++ docker/dev/back-dev/django-entrypoint.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser --noinput
+python manage.py loaddata users.json
 python manage.py runserver 0.0.0.0:8000
 
 exec "$@"
